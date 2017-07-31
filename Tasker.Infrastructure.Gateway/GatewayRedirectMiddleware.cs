0f6e62d1fba75c87163402be96e65409c7e83e5b
Grzegorz Piotrowski <gpiotrowski@users.noreply.github.com>
2017-07-31 22:06:23 +0200

    Add Gateway project with basic functionalities

---
+++ Tasker.Infrastructure.Gateway/GatewayRedirectMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Tasker.Infrastructure.Gateway
+{
+    public class GatewayRedirectMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GatewayRedirectMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            //TODO: Move this key to config file
+            var gatewayHeaderKey = "X-Gateway-Target";
+
+            if (context.Request.Headers.ContainsKey(gatewayHeaderKey))
+            {
+                var targetServer = context.Request.Headers[gatewayHeaderKey].SingleOrDefault();
+
+                var targetHost = String.Empty;
+
+                //TODO: Move this mapping to config file
+                switch (targetServer.ToLower())
+                {
+                    case "projects":
+                        targetHost = "localhost:60332";
+                        break;
+                    case "users":
+                        targetHost = "localhost:60348";
+                        break;
+                }
+
+                if (targetHost != String.Empty)
+                {
+                    var protocol = context.Request.IsHttps ? "https" : "http";
+                    context.Response.Redirect($"{protocol}://{targetHost}{context.Request.Path}");
+                }
+                else
+                {
+                    await _next.Invoke(context);
+                }
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
+        }
+    }
+}
---
+++ Tasker.Infrastructure.Gateway/GatewayRedirectMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Tasker.Infrastructure.Gateway
+{
+    public static class GatewayRedirectMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseGatewayRouting(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<GatewayRedirectMiddleware>();
+        }
+    }
+}
